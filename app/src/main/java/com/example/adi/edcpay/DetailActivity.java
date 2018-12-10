4e9141ced4cbca5166adbdcaf34b52ed3636db95
pr4sety4 <sndr1722@gmail.com>
2018-12-10 22:32:59 +0700

    +Required VIew

---
+++ app/src/main/java/com/example/adi/edcpay/DetailActivity.java
@@ -0,0 +1,17 @@
+package com.example.adi.edcpay;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrolling);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
