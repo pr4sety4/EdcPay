a6c57e0370b433f62d6520be3fd60f042e6f1df1
Pras <sndr1722@gmail.com>
2018-12-14 16:37:35 +0700

    show qr code, Divide recycler view deals and promo

--- app/src/main/java/com/example/adi/edcpay/PromoActivity.java
+++ app/src/main/java/com/example/adi/edcpay/PromoActivity.java
@@ -1,21 +1,24 @@
 package com.example.adi.edcpay;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 import com.example.adi.edcpay.adapter.PromoAdapter;
+import com.example.adi.edcpay.adapter.TabAdapter;
+import com.example.adi.edcpay.content.Deals;
+import com.example.adi.edcpay.content.Promo;
 
-import java.util.ArrayList;
-
-public class PromoActivity extends AppCompatActivity implements PromoAdapter.ItemClickListener {
+public class PromoActivity extends AppCompatActivity {
 
     RecyclerView recyclerViewPromo;
     PromoAdapter promoAdapter;
+    TabAdapter tabAdapter;
+    TabLayout tabLayout;
+    ViewPager viewPager;
     Toolbar promoListToolbar;
 
     @Override
@@ -28,17 +31,55 @@ public class PromoActivity extends AppCompatActivity implements PromoAdapter.Ite
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        ArrayList<Integer> image = new ArrayList<>();
-        image.add(R.drawable.ad1);
-        image.add(R.drawable.ad2);
-        image.add(R.drawable.ad3);
+        viewPager = findViewById(R.id.viewPager);
+        tabLayout = findViewById(R.id.tabLayout);
 
-        recyclerViewPromo = findViewById(R.id.homePromoRecyclerView);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
-        recyclerViewPromo.setLayoutManager(linearLayoutManager);
-        promoAdapter = new PromoAdapter(image, this);
-        promoAdapter.setClickListener(this);
-        recyclerViewPromo.setAdapter(promoAdapter);
+        int[] tabIcons = {
+                R.drawable.deals,
+                R.drawable.promo,
+        };
+
+        tabAdapter = new TabAdapter(getSupportFragmentManager(), this);
+        tabAdapter.addFragment(new Deals(), "Deals", tabIcons[0]);
+        tabAdapter.addFragment(new Promo(), "Promo", tabIcons[1]);
+
+
+        viewPager.setAdapter(tabAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+
+        highLightCurrentTab(0);
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                highLightCurrentTab(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+    }
+
+    private void highLightCurrentTab(int position) {
+
+        for (int i = 0; i < tabLayout.getTabCount(); i++) {
+            TabLayout.Tab tab = tabLayout.getTabAt(i);
+            assert tab != null;
+            tab.setCustomView(null);
+            tab.setCustomView(tabAdapter.getTabView(i));
+        }
+
+        TabLayout.Tab tab = tabLayout.getTabAt(position);
+        assert tab != null;
+        tab.setCustomView(null);
+        tab.setCustomView(tabAdapter.getSelectedTabView(position));
 
     }
 
@@ -48,9 +89,9 @@ public class PromoActivity extends AppCompatActivity implements PromoAdapter.Ite
         return true;
     }
 
-    @Override
-    public void onItemClick(View view, int position) {
-        Intent intent = new Intent(this, DetailActivity.class);
-        startActivity(intent);
-    }
+//    @Override
+//    public void onItemClick(View view, int position) {
+//        Intent intent = new Intent(this, DetailActivity.class);
+//        startActivity(intent);
+//    }
 }
---
+++ app/src/main/java/com/example/adi/edcpay/content/Deals.java
@@ -0,0 +1,58 @@
+package com.example.adi.edcpay.content;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.adi.edcpay.DetailActivity;
+import com.example.adi.edcpay.R;
+import com.example.adi.edcpay.adapter.PromoAdapter;
+
+import java.util.ArrayList;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Deals extends Fragment implements PromoAdapter.ItemClickListener {
+
+
+    RecyclerView recyclerViewPromo;
+    PromoAdapter promoAdapter;
+
+    public Deals() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_deals, container, false);
+
+        ArrayList<Integer> image = new ArrayList<>();
+        image.add(R.drawable.ad1);
+        image.add(R.drawable.ad2);
+        image.add(R.drawable.ad3);
+
+        recyclerViewPromo = view.findViewById(R.id.homePromoRecyclerView);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
+        recyclerViewPromo.setLayoutManager(linearLayoutManager);
+        promoAdapter = new PromoAdapter(image, getActivity());
+        promoAdapter.setClickListener(this);
+        recyclerViewPromo.setAdapter(promoAdapter);
+
+        return view;
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        Intent intent = new Intent(getActivity(), DetailActivity.class);
+        startActivity(intent);
+    }
+}
